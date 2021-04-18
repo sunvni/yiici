dfcf244178cc8c4f783b639a84ba459cb8ad00c9
Hoang Hieu <sunvni@gmail.com>
2021-04-18 12:07:58 +0900

    update db setting

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '123456',
+    'dsn' => 'mysql:host=yiici_php_1;dbname=yiici',
+    'username' => 'yiici',
+    'password' => 'yiici',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
